dbb74a4b00d7f36f8b0baff29d7c6eee6a8bab14
Alan Vazquez <82201194+Vazpei@users.noreply.github.com>
2025-03-05 15:45:33 -0600

    New components created

--- helarte-web/src/App.jsx
+++ helarte-web/src/App.jsx
@@ -1,11 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header.jsx'
+import './style/Header.css'
+import Home from './components/Home.jsx';
+import Products from './components/Products.jsx';
+import Contact from './components/Contact.jsx';
+import { useState } from 'react';
 
 function App() {
+
+  const [page,setPage] = useState("home");
+
   return (
     <div className="App">
-      <Header/>
+      <div className="header-container">
+            <h2 className="title">RockPizza</h2>
+            <nav className="navegation">
+                <button onClick={() => setPage("home")}>Inicio</button>
+                <button onClick={() => setPage("products")}>Productos</button>
+                <button onClick={() => setPage("contact")}>Contactanos</button>
+                <button onClick={() => setPage("location")}>Ubicacion</button>
+            </nav>
+      </div>
+      <main>
+        {page === "home" && <Home/>}
+        {page === "contact" && <Contact/>}
+        {page === "products" && <Products/>}
+      </main>
     </div>
   );
 }
