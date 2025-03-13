50745e3b8974bc702cb7a276dfd5d068ec164a6b
Alan Vazquez <82201194+Vazpei@users.noreply.github.com>
2025-03-13 05:09:35 -0600

    Banner component added

---
+++ portfolio/src/components/Banner.jsx
@@ -0,0 +1,39 @@
+import './../styles/Banner.css'
+import linkedinLogo from './../img/linkedin.png'
+import gitLogo from './../img/git.png'
+export default function Banner() {
+    return(
+        <>
+            <div className="banner-container">
+                <div className="banner-info-container">
+                    <div className="banner-title">
+                        <h2>Alan Vazquez</h2>
+                    </div>
+                    <div className="banner-skills">
+                        <h2>Developer</h2>
+                    </div>
+                    <div className="banner-info">
+                        <p>Being part of a company where I can put into practice all my
+                        knowledge about the technologies I master, have the opportunity to
+                        achieve my medium and long-term goals as well as develop my work,
+                        personal and intellectual skills.
+                        </p>
+                    </div>
+                </div>
+                <div className="banner-interactive">
+                    <div className="banner-button">
+                        <button>Download CV</button>
+                    </div>
+                    <div className="banner-icons-container">
+                        <div className="linkedin-icon">
+                            <img src={linkedinLogo} alt="Linkedin" />
+                        </div>
+                        <div className="Git-icon">
+                            <img src={gitLogo} alt="github" onClick={() => alert('GitHub')}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+};
