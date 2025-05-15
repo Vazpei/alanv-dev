c35d146368468c677dd869e491ca00f8209e4f38
Alan Vazquez <82201194+Vazpei@users.noreply.github.com>
2025-05-15 16:15:32 -0600

    App modified by custom hook useCart /tech-store

--- tech-store/src/App.jsx
+++ tech-store/src/App.jsx
@@ -4,29 +4,12 @@ import Navegation from './components/Navegation.jsx'
 import Content from './components/Content.jsx'
 import Footer from './components/Footer.jsx'
 import Cart from './components/Cart.jsx'
-import { useMemo } from 'react'
+import { useCart } from './hooks/useCart.js'
 
 function App() {
+
+  const { cart,total,setCart,setTotal,addToCart } = useCart();
   const [page,setPage] = useState("Home");
-  const [cart,setCart] = useState([]);
-  const [total,setTotal] = useState("0");
-
-  function addToCart(item){
-    const { id, price } = item;
-    const itemExists = cart.findIndex(itemCart => itemCart.id === id);
-
-    if(itemExists >= 0){//cart exist just add to qty
-      const updatedCart = [...cart]//Exactly of current cart
-      updatedCart[itemExists].quantity++;//Updating cart quant
-      setCart(updatedCart);//setting the cart copy with the old one to apply changes on state
-    
-    }else{// not exists on cart then create a new one with the prev cart
-      setCart(prevCart => [...prevCart,item])//copiamos el previo y agregamos nuevo item
-      item.quantity = 1;
-  
-    }
-    console.log(itemExists);
-  }
 
   return (
     <>
