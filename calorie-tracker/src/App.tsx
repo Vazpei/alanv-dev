6808d8a80d32896f4203affd34f5fefd3adfe513
Alan Vazquez <82201194+Vazpei@users.noreply.github.com>
2025-05-30 11:22:58 -0600

    New project Calorie tracker added

---
+++ calorie-tracker/src/App.tsx
@@ -0,0 +1,18 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center text-lg font-bold text-white uppercase p-1">
+            Contador de calorias
+          </h1>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default App
