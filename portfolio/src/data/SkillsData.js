76f617376d5745111c4ade81134573807418de32
Alan Vazquez <82201194+Vazpei@users.noreply.github.com>
2025-06-03 08:44:06 -0600

    Update SkillsData.js/ portfolio
    Updating technologies

--- portfolio/src/data/SkillsData.js
+++ portfolio/src/data/SkillsData.js
@@ -3,73 +3,73 @@ export const SkillsData = [
     {
         id:1,
         name:'UiPath RPA',
-        description:'I use this technologie to can design the core of web applications.',
+        description:'Modern technologie for RPA applications development.',
         icon:'/img/uipath.webp'
     },
     {
         id:2,
         name:'HTML',
-        description:'I use this technologie to can design the core of web applications.',
+        description:'Technologie for structure design of web apps.',
         icon:'/img/html.png'
     },
     {
         id:3,
         name:'CSS',
-        description:'I use this technologie to put designs and styles into my web apps.',
+        description:'Technologie to put custom designs and styles into my web apps.',
         icon:'/img/css-3.png'
     },
     {
         id:4,
         name:'JavaScript',
-        description:'I use this technologie to put designs and styles into my web apps.',
+        description:'Technologie to implement functions and methods within web apps.',
         icon:'/img/js.png'
     },
     {
         id:5,
         name:'React Js',
-        description:'I use this technologie to put designs and styles into my web apps.',
+        description:'Modern technologie for web components and states for web apps.',
         icon:'/img/science.png'
     },
     {
         id:6,
         name:'TypeScript',
-        description:'I use this technologie to put designs and styles into my web apps.',
+        description:'Type coding for better variable use in web apps.',
         icon:'/img/typescript.png'
     },
     {
         id:7,
         name:'GitHub',
-        description:'I use this technologie to put designs and styles into my web apps.',
+        description:'Version control technologie to create, save and share code in teamwork.',
         icon:'/img/github.png'
     },
     {
         id:8,
         name:'MySql',
-        description:'I use this technologie to put designs and styles into my web apps.',
+        description:'Query language for manage data for differents applications.',
         icon:'/img/mysql-database.png'
     },
     {
         id:9,
         name:'C#',
-        description:'I use this technologie to put designs and styles into my web apps.',
+        description:'Object-oriented programming language developed by Microsoft that runs on the .NET Framework i used it for Windows forms apps',
         icon:'/img/c-sharp.png'
     },
     {
         id:10,
         name:'Photoshop',
-        description:'I use this technologie to put designs and styles into my web apps.',
+        description:'Photo editor with layout technologie to can edit any type picture.',
         icon:'/img/photoshop.png'
     },
     {
         id:11,
         name:'Tailwind',
-        description:'I use this technologie to put designs and styles into my web apps.',
+        description:'Modern and advanced technologie for web development style.',
         icon:'https://www.svgrepo.com/show/374118/tailwind.svg'
     },
     {
         id:12,
         name:'Java',
-        description:'I use this technologie to put designs and styles into my web apps.',
+        description:'Object-oriented programming language for high level.',
         icon:'https://cdn.worldvectorlogo.com/logos/java.svg'
     }
 ]
