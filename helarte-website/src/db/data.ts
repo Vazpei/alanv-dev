5f57b182cf168b41f3b77cb9f87019127044a3dd
Alan Vazquez <82201194+Vazpei@users.noreply.github.com>
2025-07-30 19:28:20 -0600

    Changes on helarte-weebsite

---
+++ helarte-website/src/db/data.ts
@@ -0,0 +1,14 @@
+import type { Product } from "../types/Product"
+
+const db:Product[] = [
+    {
+        name:'Paleta de fresa',
+        description:'Paleta congelada de frambuesa',
+        price:324,
+        img:''
+    }
+] 
+
+export {
+    db
+}
